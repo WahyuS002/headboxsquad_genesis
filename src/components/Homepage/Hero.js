a1599fa752d27cd3c3fef58193fb7be126415bb7
WahyuS002 <syahputrawahyu61@gmail.com>
2022-03-19 15:53:56 +0700

    :zap: quick change total supply from 7,777 to 999

--- src/components/Homepage/Hero.js
+++ src/components/Homepage/Hero.js
@@ -8,8 +8,8 @@ export default function Hero() {
                 <span className="text-center text-5xl md:text-7xl font-bold text-primary-90 absolute inset-0 right-2 top-1 -z-10">Discover Every Box</span>
             </div>
             <div className="relative mt-12">
-                <h5 className="text-center text-3xl font-bold text-white z-10">7,777 Boxes in Headbox Squad</h5>
-                <span className="text-center text-3xl font-bold text-primary-90 absolute inset-0 right-[0.3rem] top-[0.1rem] -z-10">7,777 Boxes in Headbox Squad</span>
+                <h5 className="text-center text-3xl font-bold text-white z-10">999 Boxes in Headbox Squad</h5>
+                <span className="text-center text-3xl font-bold text-primary-90 absolute inset-0 right-[0.3rem] top-[0.1rem] -z-10">999 Boxes in Headbox Squad</span>
             </div>
             <div className="mt-12 flex justify-center">
                 <a href="https://mint.headboxsquad.com" className="bg-primary-90 hover:shadow-xl transition-all duration-300 ease-in-out text-white font-bold px-12 py-4 rounded-md">
